226c344c99b2927fbac4dc68f8542ae5daa5b2ee
ankita13406 <158020715+ankita13406@users.noreply.github.com>
2026-03-08 19:50:19 +0530

    Update blink_generator.v

--- blink_generator.v
+++ blink_generator.v
@@ -1,17 +1,16 @@
 module blink_generator(
     input clk,
     input invalid,
-
     output reg blink_led
 );
 
-// Toggle LED for blinking
+// toggle LED when invalid operation occurs
 always @(posedge clk)
 begin
     if(invalid)
-        blink_led <= ~blink_led;  // Blink when invalid operation
+        blink_led <= ~blink_led;
     else
-        blink_led <= 0;           // LED OFF otherwise
+        blink_led <= 0;
 end
 
 endmodule
