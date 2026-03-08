bc1810e24dca1b7fc494ed9a31bf6406aecd7c4a
ankita13406 <158020715+ankita13406@users.noreply.github.com>
2026-03-08 19:48:20 +0530

    Update arithmetic_unit.v

--- arithmetic_unit.v
+++ arithmetic_unit.v
@@ -3,7 +3,6 @@ module arithmetic_unit(
     input [3:0] b,
     input [1:0] m,
     input enable,
-
     output reg [7:0] y,
     output reg invalid
 );
@@ -12,29 +11,28 @@ always @(*)
 begin
     if(enable == 0)
     begin
-        y = 0;            // Disable arithmetic if locked
+        y = 0;
         invalid = 0;
     end
-
     else
     begin
         case(m)
 
         2'b01:
         begin
-            y = a + b;    // Perform addition
+            y = a + b;        // addition
             invalid = 0;
         end
 
         2'b10:
         begin
-            y = a * b;    // Perform multiplication
+            y = a * b;        // multiplication
             invalid = 0;
         end
 
         default:
         begin
-            y = 0;        // No result for invalid mode
+            y = 0;            // invalid operation
             invalid = 1;
         end
 
