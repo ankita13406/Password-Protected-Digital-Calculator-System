fcb6951f0664032f27143de92b2f77502b00b51f
ankita13406 <158020715+ankita13406@users.noreply.github.com>
2026-03-08 19:41:59 +0530

    Create clock_divider.v

---
+++ clock_divider.v
@@ -0,0 +1,29 @@
+module clock_divider(
+    input clk,        // 100 MHz clock from Basys3
+    input reset,      // asynchronous reset
+    output reg clk_out
+);
+
+// Counter to divide 100 MHz clock
+reg [25:0] count;    // 26-bit counter is enough for 50M
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+    begin
+        count <= 0;       // reset counter
+        clk_out <= 0;     // reset output clock
+    end
+    else
+    begin
+        if(count == 50_000_000)   // 0.5 sec delay for 2 Hz blink
+        begin
+            clk_out <= ~clk_out;  // toggle slow clock
+            count <= 0;           // restart counting
+        end
+        else
+            count <= count + 1;   // increment counter
+    end
+end
+
+endmodule
