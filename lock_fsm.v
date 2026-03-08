457231ffd29530fe4fcf32611f95c5f54fe75a19
ankita13406 <158020715+ankita13406@users.noreply.github.com>
2026-03-08 19:47:49 +0530

    Update lock_fsm.v

--- lock_fsm.v
+++ lock_fsm.v
@@ -1,27 +1,27 @@
 module lock_fsm(
     input clk,
     input reset,
-    input S1, S2, S3,
-
+    input S1,
+    input S2,
+    input S3,
     output reg unlocked,
     output reg error
 );
 
-// State encoding
+// FSM states
 reg [2:0] state;
 
-parameter IDLE  = 3'b000;
-parameter ST1   = 3'b001;
-parameter ST2   = 3'b010;
-parameter ST3   = 3'b011;
-parameter UNLOCK= 3'b100;
-parameter ERR   = 3'b101;
+parameter IDLE   = 3'b000;
+parameter ST1    = 3'b001;
+parameter ST2    = 3'b010;
+parameter UNLOCK = 3'b011;
+parameter ERR    = 3'b100;
 
 always @(posedge clk or posedge reset)
 begin
     if(reset)
     begin
-        state <= IDLE;        // Reset system to initial state
+        state <= IDLE;
         unlocked <= 0;
         error <= 0;
     end
@@ -33,30 +33,32 @@ begin
         begin
             unlocked <= 0;
             error <= 0;
-            if(S3) state <= ST1;     // First correct button
-            else if(S1 | S2) state <= ERR;
+            if(S3) state <= ST1;
+            else if(S1 || S2) state <= ERR;
         end
 
         ST1:
         begin
-            if(S1) state <= ST2;     // Second correct button
-            else if(S2 | S3) state <= ERR;
+            if(S1) state <= ST2;
+            else if(S2 || S3) state <= ERR;
         end
 
         ST2:
         begin
-            if(S2) state <= UNLOCK;  // Final correct button
-            else if(S1 | S3) state <= ERR;
+            if(S2) state <= UNLOCK;
+            else if(S1 || S3) state <= ERR;
         end
 
         UNLOCK:
         begin
-            unlocked <= 1;           // Calculator enabled
+            unlocked <= 1;     // calculator enabled
+            state <= UNLOCK;
         end
 
         ERR:
         begin
-            error <= 1;              // Wrong sequence detected
+            error <= 1;        // error state
+            state <= ERR;
         end
 
         endcase
